9cb800b8376c3651e6fe186aea0e2eb6677aa976
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-14 19:52:43 +0530

    new

---
+++ src/main/java/com/example/SpringBoot/LearningSpringBoot/LearningSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.example.SpringBoot.LearningSpringBoot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringBootApplication.class, args);
+
+	}
+
+
+}
---
+++ src/test/java/com/example/SpringBoot/LearningSpringBoot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot.LearningSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
