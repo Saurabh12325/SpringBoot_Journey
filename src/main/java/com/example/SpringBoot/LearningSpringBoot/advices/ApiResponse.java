822f384af5037ecceed967d92b7164ffce1e6f7d
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-04-18 14:47:56 +0530

    changes

---
+++ src/main/java/com/example/SpringBoot/LearningSpringBoot/advices/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.example.SpringBoot.LearningSpringBoot.advices;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Data
+public class ApiResponse<T> {
+
+    private LocalDateTime timestamp;
+
+
+    private T data;
+    private ApiError error;
+
+    public ApiResponse(ApiError error) {
+        this();
+        this.error = error;
+    }
+
+
+
+
+    public ApiResponse(T data) {
+        this();
+        this.data = data;
+    }
+
+    public ApiResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+
+}
